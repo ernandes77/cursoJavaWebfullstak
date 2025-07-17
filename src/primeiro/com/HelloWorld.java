ca67fc1bb3c807fb1a329fb299a24d0698e12439
Ernandes <ernandes77@hotmail.com>
2025-07-17 08:59:35 -0300

    commit inicial

---
+++ src/primeiro/com/HelloWorld.java
@@ -0,0 +1,16 @@
+package primeiro.com;
+
+
+public class HelloWorld {
+
+	
+	public static void main (String[] args){
+		/*entrada de dados variaveis*/
+		int a = 2;//variavel atributo e valor
+		int b =10;
+		int c = a+b;
+		
+		//escreve na tela
+	System.out.println("Resultado A + B = "+c);
+	}
+}
