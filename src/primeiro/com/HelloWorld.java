2ef78b94631fd7d4a6d61707f893a2e7f4a63821
Ernandes <ernandes77@hotmail.com>
2025-07-17 09:20:07 -0300

    primeiro.com

--- src/primeiro/com/HelloWorld.java
+++ src/primeiro/com/HelloWorld.java
@@ -6,9 +6,10 @@ public class HelloWorld {
 	
 	public static void main (String[] args){
 		/*entrada de dados variaveis*/
+		
 		int a = 2;//variavel atributo e valor
 		int b =10;
-		int c = a+b;
+		int c = a+b;//SOMA A +B
 		
 		//escreve na tela
 	System.out.println("Resultado A + B = "+c);
