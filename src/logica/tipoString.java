329c3cb74bb772a7883594fa3ee5cb8883db7696
Ernandes <ernandes77@hotmail.com>
2025-07-18 14:19:38 -0300

    String

---
+++ src/logica/tipoString.java
@@ -0,0 +1,17 @@
+package logica;
+
+public class tipoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nome = "Ernandes";
+		String cpf = "021.124.587.63";
+		String telefone = "6199654789";
+		String endereco = "ar12 conjunti 12 casa 06";
+		
+		System.out.println("Nome " + nome +" cpf "+ cpf +" telefone "+telefone+" endereco "+endereco);
+
+	}
+
+}
