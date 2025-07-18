48725ccaef23f859b55ac0e73ef7095fc153bc83
Ernandes <ernandes77@hotmail.com>
2025-07-18 10:33:52 -0300

    iniciando

---
+++ src/logica/TiposVariaveis.java
@@ -0,0 +1,18 @@
+package logica;
+
+public class TiposVariaveis {
+	
+	public static void main (String[]args) {
+		
+		int idade = 10;
+		float preco = 120;
+		double quebrado = 12;
+		String cpf = "020.254.021.35";
+		
+		System.out.println("idade =" +idade);
+		System.out.println("preco ="+preco);
+		System.out.println("quebrado = "+preco);
+		System.out.println("cpg = "+ cpf);
+	}
+
+}
