2364b707376414e6b07312a26dbd6bbc73e97ec5
Ernandes <ernandes77@hotmail.com>
2025-07-18 13:50:00 -0300

    calculos con inteiros media

---
+++ src/logica/Calculos.java
@@ -0,0 +1,17 @@
+package logica;
+
+public class Calculos {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 1;
+		int nota2 = 5;
+		int nota3 = 7;
+		int nota4 = 9;
+		
+		int soma = (nota1+nota2+nota3+nota4)/4;
+		System.out.println("media = "+soma);
+
+	}
+
+}
