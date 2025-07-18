26f8a702a9d1b08f98ad5319c8a21b8b9095bede
Ernandes <ernandes77@hotmail.com>
2025-07-18 14:06:42 -0300

    caracteres

---
+++ src/logica/Caracter.java
@@ -0,0 +1,24 @@
+package logica;
+
+public class Caracter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char sexo ='m';
+		char pessoaFisica= 'm';
+		char pessoajuridica ='f';
+		char pessoaMasculina = 'm';
+		char pessoaFemenina ='f';
+		
+		if(sexo == 'm') {
+			System.out.println("Masculino");
+		}else {
+			System.out.println("femenino");
+				}
+		
+		System.out.println("sexo = "+sexo);
+		
+	}
+
+}
