e5741989520e063ad438b81865687d43802fbcad
Ernandes <ernandes77@hotmail.com>
2025-07-22 08:49:21 -0300

    subindo

--- src/logica/MediaAluno.java
+++ src/logica/MediaAluno.java
@@ -5,15 +5,28 @@ public class MediaAluno {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int media = 20;
+		/*
+		 * int media = 20;
+		 * 
+		 * if (media == 70) { System.out.println("Aluno aprovado = " + media);//calcula
+		 * a media e mostrara aprovado
+		 * 
+		 * } else if (media >= 40 && media <= 69) {
+		 * System.out.println("Aluno recuperacao = " + media);//calcula a media e
+		 * mostrara recuperacao } else
+		 * System.out.println("Aluno nao encontrado ");//caso nao cai em nenhum mostra
+		 * aluno nao encontrad
+		  }*/
 
-		if (media == 70) {
-			System.out.println("Aluno aprovado = " + media);
+	// operadores ternarios
 
-		} else if (media >= 40 && media <= 69) {
-			System.out.println("Aluno recuperacao = " + media);
-		} else
-			System.out.println("Aluno nao encontrado ");
+	int nota1 = 10;
+	int nota2 = 100;
+	int media = nota1 + nota2;
+
+	String saidaDeDados= media >=70 ? "APROVADO" : (media >=40 && media <=69) ? "aluno recuperacao " : "aluno reprovado";
+	
+	System.out.println(saidaDeDados);
+	
 	}
-
 }
