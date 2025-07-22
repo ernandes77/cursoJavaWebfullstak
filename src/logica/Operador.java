d82ebacf631b8484e2a9c04810b5a0e2ad90a44a
Ernandes <ernandes77@hotmail.com>
2025-07-22 13:45:55 -0300

    operadores logicos

---
+++ src/logica/Operador.java
@@ -0,0 +1,24 @@
+package logica;
+
+public class Operador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int nota1 = 30;
+int nota2 = 30;
+//igual
+if(nota1 == nota2) {
+	System.out.println("notas iguais");
+}else {
+	System.out.println("notas diferentes");
+}
+//diferentes
+	if(nota1 != nota2) {
+		System.out.println("notas diferentes");
+	}else {
+		System.out.println("notas iguais");
+	}
+	}
+	
+
+}
